953170008f296e764f70caaeeb56d4eb790f6fea
Unzzui <98267393+Unzzui@users.noreply.github.com>
2022-03-20 11:18:50 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "[server]"  > ~/.streamlit/config.toml 
-echo "headless = true"  >> ~/.streamlit/config.toml
-echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = false"  >> ~/.streamlit/config.toml
-echo "[theme]" > ~/.streamlit/config.toml
-echo "base = light" >> ~/.streamlit/config.toml
+echo "[theme]
+base = light
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
